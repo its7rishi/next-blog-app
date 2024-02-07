bdcde90ab523dbe7c0dd74b41b8364c483298160
Saptarshi Majumdar <saptarshi.maj@yahoo.com>
2024-02-07 13:36:14 +0530

    Blog Detail Page completed

--- app/blogs/page.jsx
+++ app/blogs/page.jsx
@@ -1,4 +1,25 @@
-const Blogs = () => {
-  return <div>Blogs</div>;
+import { fetchBlogs } from "@/actions/actions";
+import { PrismaClient } from "@prisma/client";
+import BlogItem from "../components/BlogItem";
+
+const prisma = new PrismaClient();
+
+const Blogs = async () => {
+  const blogs = await prisma.blog.findMany({});
+
+  return (
+    <div>
+      <h2 className="text-center mt-4 px-2 text-2xl py-2 font-bold">
+        All Blogs
+      </h2>
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 mt-5 mb-5 px-4 py-5">
+        {blogs?.length > 0 &&
+          blogs?.map((blog) => {
+            return <BlogItem key={blog?.id} blog={blog} />;
+          })}
+      </div>
+    </div>
+  );
 };
 export default Blogs;
---
+++ app/components/BlogItem.jsx
@@ -0,0 +1,34 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const BlogItem = ({ blog }) => {
+  const { id, imageUrl, title, description, category } = blog || {};
+  return (
+    <div className="bg-gray-900 p-4 border-2 border-green-200 mx-2 my-2 rounded-lg shadow-md">
+      <Link href={`/blogs/${id}`}>
+        {imageUrl ? (
+          <Image
+            blurDataURL={imageUrl}
+            placeholder="blur"
+            loading="lazy"
+            quality={100}
+            src={imageUrl}
+            width={600}
+            height={400}
+            className="w-full h-[200px] lg:h-[250px] object-cover mb-4 rounded-md"
+          />
+        ) : null}
+      </Link>
+
+      <Link href={`/blogs/${id}`}>
+        <h2 className="text-xl text-white font-semibold mb-2">{title}</h2>
+      </Link>
+
+      <p className="mb-2 max-w-md text-green-500 inline-block border-2 p-2 border-green-300 rounded-full">
+        {category}
+      </p>
+      <p className="text-gray-epp">{description}</p>
+    </div>
+  );
+};
+export default BlogItem;
